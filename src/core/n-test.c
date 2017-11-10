b12fc664fd5bb1f59b264e1b9d1883da5454ecb2
Oldes <oldes.huhuman@gmail.com>
2017-11-10 13:52:10 +0100

    TEST: just a demonstration what must be done to create a new native function in its own C file (using a special header in C file)

---
+++ src/core/n-test.c
@@ -0,0 +1,44 @@
+/***********************************************************************
+**
+**  REBOL [R3] Language Interpreter and Run-time Environment
+**
+**  Copyright 2012 REBOL Technologies
+**  REBOL is a trademark of REBOL Technologies
+**
+**  Licensed under the Apache License, Version 2.0 (the "License");
+**  you may not use this file except in compliance with the License.
+**  You may obtain a copy of the License at
+**
+**  http://www.apache.org/licenses/LICENSE-2.0
+**
+**  Unless required by applicable law or agreed to in writing, software
+**  distributed under the License is distributed on an "AS IS" BASIS,
+**  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+**  See the License for the specific language governing permissions and
+**  limitations under the License.
+**
+************************************************************************
+**
+**  Module:  n-test.c
+**  Summary: native function test
+**  Section: natives
+**  Author:  Oldes
+**
+***********************************************************************/
+
+#include "sys-core.h"
+
+/**********************************************************************/
+//
+//  test-native: native [
+//	  {This is just a native function for test purpose.}
+//	  'value [any-type!] "This can be value of any type"
+//  ]
+//
+REBNATIVE(test_native)
+{
+	REBVAL *val = D_ARG(1);
+	printf("Value has type ID: %i\n", VAL_TYPE(val));
+	return R_ARG1; //return the same value
+}
+
